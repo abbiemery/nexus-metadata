83e72c235d8e97e206d9618479517b6b9eed991d
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-11-07 17:26:39 +0000

    Remove test sql queries from main

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,5 @@ const DB_PATH: &str = "sqlite.db";
 #[tokio::main]
 async fn main() {
     let db = sqlite::SqliteService::connect(DB_PATH).await.unwrap();
-    let gql = tokio::spawn(graphql::serve_graphql(db.clone()));
-
-    // get the insertion devices from the db
-    let insertion_devices = sqlite::SqliteService::get_insertion_devices(&db)
-        .await
-        .unwrap();
-    for device in insertion_devices {
-        println!("poles: {:?}, length: {:?}", device.poles, device.length);
-    }
-
-    // Get all of the devices.
-    let device_results = sqlite::SqliteService::get_devices(&db).await.unwrap();
-    for device in device_results {
-        println!(
-            "beamline: {}, device_name: {}, uuid: {}",
-            device.beamline, device.device_name, device.uuid
-        );
-    }
-    let _ = gql.await;
+    graphql::serve_graphql(db).await;
 }
